10034b170dc1f510714fb9ecb1bf046169222326
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 12:13:05 +0545

    RecyclerView

---
+++ app/src/main/java/com/yuskay/recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.yuskay.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.ContactsAdapter;
+import model.Contacts;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Create a list of contacts
+
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Biraj ", "9809716524",R.drawable.biraj));
+        contactsList.add(new Contacts("Harry ", "9809716524",R.drawable.harry));
+        contactsList.add(new Contacts("Nikhila ", "9809716524",R.drawable.nikhila));
+        contactsList.add(new Contacts("Rajesh ", "9809716524",R.drawable.rajesh));
+        contactsList.add(new Contacts("Rinku ", "9809716524",R.drawable.rinku));
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter(this,contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+   }
+}
